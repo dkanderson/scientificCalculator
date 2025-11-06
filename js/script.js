fb81f1e164519e68df26022e03f26d11338b7ea4
dkanderson <dka.rainmaking@gmail.com>
2025-11-05 19:13:11 -0500

    Clean up, improve logic and add documentation comments

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 import calculate from "./helpers/calculate.js";
 
 (()=>{
-
+    // get dom elements
     const calculator = document.getElementById("scientific-calculator");
     const simpleToggle = document.getElementById("simple-toggle");
     const buttons = calculator.querySelectorAll("button");
 
     const State = {
         cache: [],
-        numache: [],
+        numCache: [],
         displayText: [],
         operator: [],
         expression: [],
@@ -17,13 +17,12 @@ import calculate from "./helpers/calculate.js";
         operatorOnce: false
     }
 
-    // console.log(buttons);
 
-    simpleToggle.addEventListener("click", function(){
+    simpleToggle.addEventListener("click", function(){              // toggle simple/scientific calculator
         calculator.classList.toggle("simple");
     });
 
-    buttons.forEach((button) => {
+    buttons.forEach((button) => {                                   // handle mouse click on buttons
         button.addEventListener("click", function(){
             handleClick(button.getAttribute("class"), this)
         })
@@ -32,19 +31,21 @@ import calculate from "./helpers/calculate.js";
     function handleClick(className, button){
         switch(true){
             case className === "number":
-                const num = button.getAttribute("data-number");
-                State.operatorOnce = false;
-                State.cache.push(num);
-                State.expression.push(num);
-                updateScreen(State.expression.join(""));
+                const num = button.getAttribute("data-number");     // get the number associated with the button from the data attribute
+                State.operatorOnce = false;                         // reset operator once flag ( makes sure there are no duplicate operators)
+                State.cache.push(num);                              // store each number into an array
+                State.expression.push(num);                         // store each number into the expression array
+                updateScreen(State.expression.join(""));            // update the screen with the full expression
                 break;
+
             case className === "simple-function":
                 const sfunc = button.getAttribute("data-function");
                 
                 switch(sfunc){
                     case "cancel":
+                        // Reset all values when then AC button is pressed
                         State.cache = [];
-                        State.numache = [];
+                        State.numCache = [];
                         State.expression = [];
                         State.operator = [];
                         State.inputStop = false;
@@ -63,39 +64,60 @@ import calculate from "./helpers/calculate.js";
                         console.error("Unknown function request");
                 }
                 break;
+
             case className === "special-function":
                 console.log(button);
                 break;
+
             case className === "basic-operator":
-                if(State.operatorOnce){
+                if(State.operatorOnce){                             //ensure operator is only pressed once
                     return;
                 }
 
-                const op =  button.getAttribute("data-operator");
+                const op =  button.getAttribute("data-operator");   //get the operator type from the data attribute of the button
 
-                State.operatorOnce = true;
-                State.numache.push(parseFloat(State.cache.join("")));
-                State.cache = [];
-                State.deceimalFlag = false;
-                State.operator.push( op );
-                State.expression.push(operatorSymbol(op));
-                updateScreen(State.expression.join(""));
-                if(State.operator[State.operator.length -1] === "equals"){
-                    console.log(State.numache, State.operator);
-                    updateScreen(State.expression.join(""), true);
-                    updateScreen(calculate(State.numache, State.operator));
+                if(!State.numCache.length){                         //make sure zero (0) is the default number on expressions
+                    State.cache.push("0");
+                    State.expression.push("0");
+                }
+
+                State.operatorOnce = true;                          //set operator once flag to true (operator button has been pressed once and no numbers have been entered)
+                State.numCache.push(parseFloat(State.cache.join(""))); // store the last number entered into an array
+                State.cache = [];                                   // reset the cache
+                State.deceimalFlag = false;                         // set the decimal flag to false (decimals only once)
+                State.operator.push( op );                          // store the operator in an array
+                State.expression.push(operatorSymbol(op));          // convert operator to easily understood symbol and add it to the expression array
+                updateScreen(State.expression.join(""));            // update the screen with what is in the expression array
+
+                if(State.operator[State.operator.length -1] === "equals"){  // if the operator is the equals button
+                     console.log(State.numCache, State.operator);
+                    let result = calculate(State.numCache, State.operator); // calculate result based on simple mathematical operator precedence
+                   
+                    // reset
+                    State.numCache = [];
+                    State.operator = [];
+                    State.cache = [];
+
+                    
+                    State.cache.push(result.toString());            // update cache with the result
+                    updateScreen(State.expression.join(""), true);  // move the expression to secondary display above and
+                    updateScreen(result);                           // display the result of the expression below
+                    State.operatorOnce = false;                     // reset operator once
+                    State.expression = [];                          // reset expression array
+                    State.expression.push(result.toString());       // the result becomes the new expression
                 }
                 
                 break;
+
             case className === "decimal":
-                if(State.deceimalFlag === true){
+                if(State.deceimalFlag === true){                    // make sure decimal only appears once
                     return;
                 }
-                State.deceimalFlag = true;
-                State.cache.push('.');
-                updateScreen(State.cache.join(""));
-                console.log("decimal");
+                State.deceimalFlag = true;                          // set the decimal flag once the button in pressed
+                State.cache.push('.');                              // add the decimal to the number cache
+                updateScreen(State.cache.join(""));                 // update the screen with the contents of the cahce 
                 break;
+
             default:
                 console.log("what the hell did you click");
                 return;
