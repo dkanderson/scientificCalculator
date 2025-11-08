8658f9f6f8583ff4c709c1e06ec8d1b0d815d678
dkanderson <dka.rainmaking@gmail.com>
2025-11-07 20:36:05 -0500

    add percentage function and modularize in preparation for receiving keyboard events

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import calculate from "./helpers/calculate.js";
+import handleSpecialFunction from "./helpers/handleSpecial.js";
+import handleKeyPress from "./helpers/handle_keypress.js";
 
 (()=>{
     // get dom elements
@@ -13,10 +15,11 @@ import calculate from "./helpers/calculate.js";
         operator: [],
         expression: [],
         inputStop: false,
-        deceimalFlag: false,
+        decimalFlag: false,
         operatorOnce: false,
         negativeFlag: false,
-        equated: false
+        equated: false,
+        percentage: false
     }
 
 
@@ -30,146 +33,49 @@ import calculate from "./helpers/calculate.js";
         })
     });
 
+    window.addEventListener("keydown", handleKeyPressEvent);
+
     function handleClick(className, button){
+
         switch(true){
             case className === "number":
-                
-                if(State.equated){
-                    // reset
-                    State.cache = [];
-                    State.expression = [];
-                    State.operator = [];
-                    State.numCache = [];
-                    State.equated = false;
-                    updateScreen("");
-                    updateScreen("", true);
-                }
-
-                if(State.negativeFlag){
-                    State.negativeFlag = false;                     // set negative flag to false ( next number has not been negated)
-                    State.operator.push("multiply");                // add a "multiply" operator to the operators array
-                    State.numCache.push(parseFloat(State.cache.join("")));      // update number cache with the last negated number
-                    State.cache = [];                               // clear cache
-                    State.expression.push(operatorSymbol("multiply"));          // convert operator to easily understood symbol and add it to the expression array
-                    updateScreen(State.expression.join(""));        // update screen with multiply symbol
-                }
-
                 const num = button.getAttribute("data-number");     // get the number associated with the button from the data attribute
-                State.operatorOnce = false;                         // reset operator once flag ( makes sure there are no duplicate operators)
-                State.cache.push(num);                              // store each number into an array
-                State.expression.push(num);                         // store each number into the expression array
-                updateScreen(State.expression.join(""));            // update the screen with the full expression
-               console.log(State.expression, State.cache);
+                handleNumber(num);
                 break;
 
             case className === "simple-function":
                 const sfunc = button.getAttribute("data-function");
                 
-                switch(sfunc){
-                    case "cancel":
-                        // Reset all values when then AC button is pressed
-                        State.cache = [];
-                        State.numCache = [];
-                        State.expression = [];
-                        State.operator = [];
-                        State.inputStop = false;
-                        State.deceimalFlag = false;
-                        State.operatorOnce = false;
-                        updateScreen("0");
-                        updateScreen("", true);
-                        break;
-                    case "pos-neg":
-                        if(State.expression.length && !State.negativeFlag){             // --if there is an expression and no negative flag has been set
-                            const pos = State.expression.length - State.cache.length;   // calculate offset position in the array to insert bracket and neg symbol
-                            State.cache.unshift("-");                                   // add neg symbol to the cache
-                            
-                            State.expression.splice(pos, 0, ("(-"));                    // surround last operand with parenthesis and a negative symbol (front)
-                            State.expression.push(")");                                 // (back)
-                            updateScreen(State.expression.join(""));                    // update screen with the changes 
-                            State.negativeFlag = true;                                  // set the negative flag to true (a number has been negated)
-                            console.log(State.cache);
-                        } else
-
-                        if(State.expression.length && State.negativeFlag){              // --if there is an expression and the negative flag has been set
-                            const pos = State.expression.length - ( State.cache.length + 1);    // calculate the offset position of the parenthesis for removal
-                            State.cache.shift();                                        // remove negative symbol from the cache
-
-                            State.expression.splice(pos, 1);                            // remove left bracket and neg from expression array
-                            State.expression.pop();                                     // remove right bracket from expression array
-                            updateScreen(State.expression.join(""));                    // update screen with changes
-                            State.negativeFlag = false;                                 // set the negative flag to false
-
-                            console.log("Expression: ", State.expression);
-                            console.log("Cache: ", State.cache)
-                        }
-                        
-                        console.log("pos-neg");
-                        break;
-                    case "percentage":
-                        console.log("percentage");
-                        break;
-                    default:
-                        console.error("Unknown function request");
-                }
+                handleSimpleFunction(sfunc);
                 break;
 
             case className === "special-function":
-                console.log(button);
+                const func = button.getAttribute("data-function");
+
+                handleSpecialFunction(func, State);
                 break;
 
             case className === "basic-operator":
-                if(State.operatorOnce){                             //ensure operator is only pressed once
-                    return;
-                }
-
                 const op =  button.getAttribute("data-operator");   //get the operator type from the data attribute of the button
 
-                if(!State.cache.length){                         //make sure zero (0) is the default number on expressions
-                    State.cache.push("0");
-                    State.expression.push("0");
-                }
-
-                State.operatorOnce = true;                          //set operator once flag to true (operator button has been pressed once and no numbers have been entered)
-                State.numCache.push(parseFloat(State.cache.join(""))); // store the last number entered into an array
-                State.cache = [];                                   // reset the cache
-                State.deceimalFlag = false;                         // set the decimal flag to false (decimals only once)
-                State.operator.push( op );                          // store the operator in an array
-                State.expression.push(operatorSymbol(op));          // convert operator to easily understood symbol and add it to the expression array
-                updateScreen(State.expression.join(""));            // update the screen with what is in the expression array
-
-                if(State.operator[State.operator.length -1] === "equals"){  // if the operator is the equals button
-                     console.log(State.numCache, State.operator);
-                    let result = calculate(State.numCache, State.operator); // calculate result based on simple mathematical operator precedence
-                   
-                    // reset
-                    State.numCache = [];
-                    State.operator = [];
-                    State.cache = [];
-
-                    State.equated = true;
-                    State.cache.push(result.toString());            // update cache with the result
-                    updateScreen(State.expression.join(""), true);  // move the expression to secondary display above and
-                    updateScreen(result);                           // display the result of the expression below
-                    State.operatorOnce = false;                     // reset operator once
-                    State.expression = [];                          // reset expression array
-                    State.expression.push(result.toString());       // the result becomes the new expression
-                }
-                
+                handleOperator(op);
                 break;
 
             case className === "decimal":
-                if(State.deceimalFlag === true){                    // make sure decimal only appears once
-                    return;
-                }
-                State.deceimalFlag = true;                          // set the decimal flag once the button in pressed
-                State.cache.push('.');                              // add the decimal to the number cache
-                updateScreen(State.cache.join(""));                 // update the screen with the contents of the cahce 
+                
+                handleDecimal();
                 break;
 
             default:
                 console.log("what the hell did you click");
                 return;
         }
+    }
+
+    function handleKeyPressEvent(event){
+
+        const keyObj = handleKeyPress(event);
+        console.log(keyObj);
     }
 
     function updateScreen(displayText, exp){
@@ -196,6 +102,173 @@ import calculate from "./helpers/calculate.js";
             default:
                 return;
         }
+    }
+
+    function handlePercentage(){
+
+        let index = State.operator.indexOf("%");
+        let num = State.numCache[index];
+
+        num = num / 100;
+        State.numCache.splice(index,1,num);
+        State.operator.splice(index,1);
+    
+    }
+
+    function handleSimpleFunction(sfunc){
+        switch(sfunc){
+            case "cancel":
+                // Reset all values when then AC button is pressed
+                State.cache = [];
+                State.numCache = [];
+                State.expression = [];
+                State.operator = [];
+                State.inputStop = false;
+                State.decimalFlag = false;
+                State.negativeFlag = false;
+                State.operatorOnce = false;
+                State.equated = false;
+                updateScreen("0");
+                updateScreen("", true);
+                break;
+            case "pos-neg":
+                if(State.expression.length && !State.negativeFlag){             // --if there is an expression and no negative flag has been set
+                    const pos = State.expression.length - State.cache.length;   // calculate offset position in the array to insert bracket and neg symbol
+                    State.cache.unshift("-");                                   // add neg symbol to the cache
+                    
+                    State.expression.splice(pos, 0, ("(-"));                    // surround last operand with parenthesis and a negative symbol (front)
+                    State.expression.push(")");                                 // (back)
+                    updateScreen(State.expression.join(""));                    // update screen with the changes 
+                    State.negativeFlag = true;                                  // set the negative flag to true (a number has been negated)
+                    console.log(State.cache);
+                } else
+
+                if(State.expression.length && State.negativeFlag){              // --if there is an expression and the negative flag has been set
+                    const pos = State.expression.length - ( State.cache.length + 1);    // calculate the offset position of the parenthesis for removal
+                    State.cache.shift();                                        // remove negative symbol from the cache
+
+                    State.expression.splice(pos, 1);                            // remove left bracket and neg from expression array
+                    State.expression.pop();                                     // remove right bracket from expression array
+                    updateScreen(State.expression.join(""));                    // update screen with changes
+                    State.negativeFlag = false;                                 // set the negative flag to false
+
+                    console.log("Expression: ", State.expression);
+                    console.log("Cache: ", State.cache)
+                }
+                
+                console.log("pos-neg");
+                break;
+            case "percentage":
+                State.percentage = true;
+                
+                if(!State.cache.length){                         //make sure zero (0) is the default number on expressions
+                    State.cache.push("0");
+                    State.expression.push("0");
+                    State.numCache.push(parseFloat(State.cache.join("")));
+                }
+
+                State.numCache.push(parseFloat(State.cache.join(""))); // store the last number entered into an array
+                State.cache = [];                                   // reset the cache
+                State.operator.push("%");
+                State.expression.push("%");
+                updateScreen(State.expression.join(""));
+                
+
+                console.log(State.numCache);
+                break;
+            default:
+                console.error("Unknown function request");
+        }
+    }
+
+    function handleNumber(num){
+          if(State.equated){
+                // reset
+                State.cache = [];
+                State.expression = [];
+                State.operator = [];
+                State.numCache = [];
+                State.equated = false;
+                updateScreen("");
+                updateScreen("", true);
+            }
+
+            if(State.negativeFlag){
+                State.negativeFlag = false;                     // set negative flag to false ( next number has not been negated)
+                State.operator.push("multiply");                // add a "multiply" operator to the operators array
+                State.numCache.push(parseFloat(State.cache.join("")));      // update number cache with the last negated number
+                State.cache = [];                               // clear cache
+                State.expression.push(operatorSymbol("multiply"));          // convert operator to easily understood symbol and add it to the expression array
+                updateScreen(State.expression.join(""));        // update screen with multiply symbol
+            }
+
+            
+            State.percentage = false;
+            State.operatorOnce = false;                         // reset operator once flag ( makes sure there are no duplicate operators)
+            State.cache.push(num);                              // store each number into an array
+            State.expression.push(num);                         // store each number into the expression array
+            updateScreen(State.expression.join(""));            // update the screen with the full expression
+    }
+
+    function handleOperator(op){
+        if(State.operatorOnce){                             //ensure operator is only pressed once
+            return;
+        } 
+
+        if(!State.cache.length && !State.percentage){                         //make sure zero (0) is the default number on expressions
+            State.cache.push("0");
+            State.expression.push("0");
+        }
+
+        if(State.percentage){
+            handlePercentage();
+            
+        }
+
+        State.negativeFlag = false;
+        State.operatorOnce = true;                          //set operator once flag to true (operator button has been pressed once and no numbers have been entered)
+        if(State.cache.length){
+        State.numCache.push(parseFloat(State.cache.join(""))); // store the last number entered into an array
+        }
+        State.cache = [];                                   // reset the cache
+        State.decimalFlag = false;                         // set the decimal flag to false (decimals only once)
+        State.operator.push( op );                          // store the operator in an array
+        State.expression.push(operatorSymbol(op));          // convert operator to easily understood symbol and add it to the expression array
+        updateScreen(State.expression.join(""));            // update the screen with what is in the expression array
+
+        if(State.operator[State.operator.length -1] === "equals"){  // if the operator is the equals button
+                console.log(State.numCache, State.operator);
+            let result = calculate(State.numCache, State.operator); // calculate result based on simple mathematical operator precedence
+            
+            // reset
+            State.numCache = [];
+            State.operator = [];
+            State.cache = [];
+
+            State.equated = true;
+            State.cache.push(result.toString());            // update cache with the result
+            updateScreen(State.expression.join(""), true);  // move the expression to secondary display above and
+            updateScreen(result);                           // display the result of the expression below
+            State.operatorOnce = false;                     // reset operator once
+            State.expression = [];                          // reset expression array
+            State.expression.push(result.toString());       // the result becomes the new expression
+        }
+    }
+
+    function handleDecimal(){
+        if(State.decimalFlag === true){                    // make sure decimal only appears once
+            return;
+        }
+        State.decimalFlag = true;                          // set the decimal flag once the button in pressed
+        
+        if(!State.cache.length){
+            State.cache.push("0.");
+            State.expression.push("0.");
+        } else {
+            State.cache.push('.');
+            State.expression.push('.');                              // add the decimal to the number cache
+        }
+        updateScreen(State.expression.join(""));                 // update the screen with the contents of the cahce 
     }
 
     const proxyHandler = {
