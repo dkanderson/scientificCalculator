c6ba34cdca5b738f0e9b1a0772c6716fd924214d
dkanderson <dka.rainmaking@gmail.com>
2025-12-19 23:26:07 -0500

    Create function to find offset index for expression array

--- js/helpers/utility.js
+++ js/helpers/utility.js
@@ -2,3 +2,28 @@ export function toggleActive(id, className = "active") {
   const btn = document.getElementById(id);
   btn.classList.toggle(className);
 }
+
+export function subExprLength(exp) {
+  let i = exp.length,
+    sub = 0,
+    last = "";
+
+  if (exp.length === 0) return 0;
+
+  while (i > 0) {
+    const token = exp[i];
+    sub++;
+
+    if (["&divide;", "&times;", "&minus;", "&#43;", "%"].includes(token)) {
+      last = token;
+      break;
+    }
+    i--;
+  }
+
+  return {
+    index: i,
+    last: last,
+    subCount: sub,
+  };
+}
